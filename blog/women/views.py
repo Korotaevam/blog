7685db68541ca4eb08d93e1e0105bdd99a6e4b1a
Korotaevam <112647912+Korotaevam@users.noreply.github.com>
2022-09-23 15:11:53 +0300

    Update views.py

--- blog/women/views.py
+++ blog/women/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Page aplication WOMAN')
+    return HttpResponse('Page aplication WOMAN ')
