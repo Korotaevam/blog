5b5351e65d542b5dc444088fd48a1696a33691ba
Korotaevam <kam888@mail.ru>
2022-09-29 15:32:42 +0300

    add CACHES

--- blog/women/urls.py
+++ blog/women/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, re_path
+from django.views.decorators.cache import cache_page
 from women.views import *
+
 
 urlpatterns = [
     path('', WomenHome.as_view(), name='home'),
